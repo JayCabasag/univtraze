b0972873117b8dd641dc3d4c9136a08aac70495c
Jay Cabasag <jaycabasag1999@gmail.com>
2023-12-21 21:53:02 +0800

    feat: add

--- admin/src/layouts/MainLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MainLayout({ children }) {
-  return (
-    <main>{children}</main>
-  )
-}
-
-export default MainLayout
--- admin/src/services/api/api.js
+++
@@ -1,5 +0,0 @@
-const instance = axios.create({
-    baseURL: process.env.BASE_URL,
-    timeout: 1000,
-});
-
