2e01eca4ea52094feb46d3f0a83930860268efad
Jay Cabasag <jaycabasag1999@gmail.com>
2024-03-08 20:27:39 +0800

    Add router

--- univtraze-admin-v2/server.js
+++ univtraze-admin-v2/server.js
@@ -2,19 +2,17 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
+const indexRouter = require("./routes/index")
 const userRouter = require("./routes/users")
 const authRouter = require("./routes/auth")
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index")
-});
-
+app.use("/", indexRouter)
 app.use("/users", userRouter)
 app.use("/auth", authRouter)
 
 app.listen(port, () => {
     console.clear();
-    console.log("Listeneing on port: ", port)
+    console.log("Listeneing on port:", port)
 })
